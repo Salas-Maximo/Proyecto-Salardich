b249cf9573545fad6b4b9a8e38edd3a02a3a9309
pubillax <gonzalopubill@gmail.com>
2024-07-04 17:39:55 -0300

    creacion del front y back

---
+++ salardi-app/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from './api.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  users: any[] = [];
+
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit() {
+    this.apiService.getMessage().subscribe((data: any) => {
+      this.users = data.users;
+    });
+  }
+}
